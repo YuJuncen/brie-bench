55f25d2251cae5841affcd620febb4de8b6a3973
luancheng <luancheng@pingcap.com>
2020-11-03 13:14:21 +0800

    exec: retrive log files from api_server minio address

--- ctl/lib/get_file.py
+++ ctl/lib/get_file.py
@@ -11,6 +11,7 @@ import sys
 
 # TODO allow artifacts bucket configurable
 artifacts_bucket = "artifacts"
+api_server_minio = "172.16.4.4:30812"
 
 def last_part(path: str) -> str :
     """
@@ -31,18 +32,34 @@ class FileReader:
             access_key=config.s3_access_key,
             secret_key=config.s3_secret_key,
             secure=False)
-    
-    def get_artifacts_dir_of(self, cluster_id: int) -> Object:
+        self._api_server_client = Minio(api_server_minio, secure=False)
+        self.use_self_client = True
+
+    def set_client(self, cluster_id: int):
         objs = list(self._client.list_objects(artifacts_bucket, f"{cluster_id}/"))
         if len(objs) == 0:
-            return None
+            # set client with api_server minio address to get cluster logs
+            objs = list(self._api_server_client.list_objects(artifacts_bucket, f"{cluster_id}/"))
+            if len(objs) > 0 :
+                self.use_self_client = False
+
+    def get_client(self) -> Object:
+        if self.use_self_client:
+            return self._client
+        return self._api_server_client
+
+    def get_artifacts_dir_of(self, cluster_id: int) -> Object:
+        objs = list(self.get_client().list_objects(artifacts_bucket, f"{cluster_id}/"))
+        if len(objs) == 0:
+            if len(objs) == 0:
+                return None
         if len(objs) > 1:
             return select(objs, lambda o: f"{o.object_name.strip('/')}")
         return objs[0]
 
     def select_file(self, dir: str) -> Object:
         dir = f"{dir}/" if not dir.endswith("/") else dir
-        objs : List[Object] = list(self._client.list_objects(artifacts_bucket, dir))
+        objs : List[Object] = list(self.get_client().list_objects(artifacts_bucket, dir))
         print(f"current path: {Fore.GREEN}{dir}{Fore.RESET}")
         new_obj = select(objs, lambda o: f"{last_part(o.object_name)}")
         if new_obj.is_dir:
@@ -55,7 +72,7 @@ class FileReader:
             logging.error(f"the cluster {cluster_id} seems has no artifacts, maybe it hasn't end yet?")
             exit(1)
         obj = self.select_file(artifact.object_name)
-        data = self._client.get_object(obj.bucket_name, obj.object_name)
+        data = self.get_client().get_object(obj.bucket_name, obj.object_name)
         proc = subprocess.Popen(["less"], stdin = subprocess.PIPE)
         # this would load the total file into memory, if the file is big,
         # we might meet some error, but don't worry for now.
@@ -63,12 +80,13 @@ class FileReader:
 
     def run(self):
         cluster = from_cli_desc(shift())
+        self.set_client(cluster)
         file = shift()
         if file is None:
             self.query_file(cluster)
             return
         artifact = self.get_artifacts_dir_of(cluster)
-        obj = self._client.get_object(artifacts_bucket, f"{artifact.object_name}{file}")
+        obj = self.get_client().get_object(artifacts_bucket, f"{artifact.object_name}{file}")
         try:
             for d in obj.stream(32 * 1024):
                     sys.stdout.write(d.decode('utf-8'))
